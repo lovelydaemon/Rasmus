fdcbcb0ec74cdb1b6fb6e327051b1fad5f4e292c
lovelydaemon <slamy777@gmail.com>
2024-03-14 12:39:31 +0300

    feat: add global prefix `api`

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,9 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  app.setGlobalPrefix('api');
   app.use(compression());
 
   console.log(`App started on http://${process.env.HOST}:${process.env.PORT}`);
-  await app.listen(process.env.PORT);
+  await app.listen(+process.env.PORT);
 }
 bootstrap();
