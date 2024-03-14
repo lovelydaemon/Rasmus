f70e18e4c1c7bdc6387968f484d594f54159b92f
lovelydaemon <slamy777@gmail.com>
2024-03-14 14:22:00 +0300

    feat: save channel and posts to db on search

--- server/src/rss/rss.controller.ts
+++ server/src/rss/rss.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RssService } from './rss.service';
+import { Channel, Post as ChannelPost } from '@prisma/client';
 
 @Controller()
 export class RssController {
   constructor(private readonly rssService: RssService) {}
 
   @Post('/')
-  getRss(@Body('url') url: string) {
+  getRss(
+    @Body('url') url: string,
+  ): Promise<{ channel: Channel; posts: ChannelPost[] }> {
     return this.rssService.parseRssUrl(url);
   }
 }
--- server/src/rss/rss.service.ts
+++ server/src/rss/rss.service.ts
@@ -2,9 +2,12 @@ import { Injectable } from '@nestjs/common';
 import * as Parser from 'rss-parser';
 import * as sanitizeHtml from 'sanitize-html';
 import { sanitizeHtmlConfig } from 'src/config/sanitize-html.config';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RssService {
+  constructor(private prisma: PrismaService) {}
+
   private readonly parser = new Parser();
 
   async parseRssUrl(url: string) {
@@ -15,14 +18,58 @@ export class RssService {
       return null;
     }
 
-    parsed.items = parsed.items.map((item) => {
-      try {
-        const content = sanitizeHtml(item.content, sanitizeHtmlConfig);
-        return { ...item, content };
-      } catch {
-        return { ...item, content: '' };
-      }
+    let shortLink = '';
+    try {
+      shortLink = new URL(parsed.link).hostname.replace('www.', '');
+    } catch {
+      shortLink = '';
+    }
+
+    const channel = await this.prisma.channel.upsert({
+      where: {
+        link: parsed.link,
+      },
+      update: {
+        title: parsed.title,
+        link: parsed.link,
+        shortLink,
+      },
+      create: {
+        link: parsed.link,
+        title: parsed.title,
+        shortLink,
+      },
     });
-    return parsed;
+
+    const posts = parsed.items.map((item) => {
+      let description: string;
+
+      try {
+        description = sanitizeHtml(item.content, sanitizeHtmlConfig);
+      } catch {
+        description = '';
+      }
+
+      return {
+        title: item.title,
+        description,
+        link: item.link,
+        date: item.isoDate,
+        channelId: channel.id,
+      };
+    });
+
+    await this.prisma.post.createMany({
+      data: posts,
+      skipDuplicates: true,
+    });
+
+    const channelPosts = await this.prisma.post.findMany({
+      where: {
+        channelId: channel.id,
+      },
+    });
+
+    return { channel: channel, posts: channelPosts };
   }
 }
