e7674413ffa086963d612672f761db0d18ff1652
lovelydaemon <slamy777@gmail.com>
2024-03-16 17:34:12 +0300

    feat: add react rules, scss, remove emotion styles

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/colors";
+          @import "./src/styles/animations";
+        `,
+      },
+    },
   },
   resolve: {
     alias: {
